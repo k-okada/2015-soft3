80e62b958906e7829081adb64e9aaab95162ba27
mech-user <kuroshibashion@gmail.com>
2015-10-05 23:12:48 +0900

    changed begineer to beginner in build and devel

--- 20150930/build/beginner_tutorials/CMakeFiles/_begineer_tutorials_generate_messages_check_deps_Hello.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/_begineer_tutorials_generate_messages_check_deps_Hello"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/_begineer_tutorials_generate_messages_check_deps_Hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- 20150930/build/beginner_tutorials/CMakeFiles/begineer_tutorials_generate_messages_lisp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/begineer_tutorials_generate_messages_lisp"
-  "/home/mech-user/2015winter/soft3/2015-soft3/20150930/devel/share/common-lisp/ros/begineer_tutorials/msg/Hello.lisp"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/begineer_tutorials_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
