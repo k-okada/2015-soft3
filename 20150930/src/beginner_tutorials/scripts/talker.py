eb8141d55231df5b6b7276a2a99432aa88f298a6
kiehoriuchi <kmkn.1109@gmail.com>
2015-10-07 20:59:13 +0900

    add 2015-09-20 shukudai

--- 20150930/src/beginner_tutorials/scripts/talker.py
+++ 20150930/src/beginner_tutorials/scripts/talker.py
@@ -3,14 +3,14 @@ import roslib
 import rospy
 from std_msgs.msg import String
 def talker():
-    pub = rospy.Publisher('chatter', String)
+    pub=rospy.Publisher('chatter',String)
     rospy.init_node('talker')
     while not rospy.is_shutdown():
-        str = "hello world %s"%rospy.get_time()
+        str="hello world %s"%rospy.get_time()
         rospy.loginfo(str)
         pub.publish(String(str))
         rospy.sleep(1.0)
-if __name__ == '__main__':
+if __name__=='__main__':
     try:
         talker()
     except rospy.POSInterruptException: pass
--- 20150930/src/beginner_tutorials/scripts/talker2.py
+++ 20150930/src/beginner_tutorials/scripts/talker2.py
@@ -5,20 +5,20 @@ from std_msgs.msg import String
 from beginner_tutorials.msg import Hello
 
 def talker2():
-    pub = rospy.Publisher('chatter2', Hello)
+    pub=rospy.Publisher('chatter2',Hello)
     rospy.init_node('talker')
     while not rospy.is_shutdown():
         str = "hello world %s"%rospy.get_time()
         rospy.loginfo(str)
         hello = Hello();
         hello.hello = "world"
-        hello.pos.x = 0
-        hello.pos.y = 1
-        hello.pos.z = 2
-        pub.publish(hello)
+        hello.pos.x = 0;
+        hello.pos.y = 1;
+        hello.pos.z = 2;
+        pub.publish(hello);
         rospy.sleep(1.0)
 
-if __name__ == '__main__':
+if __name__=='__main__':
     try:
         talker2()
     except rospy.POSInterruptException: pass
