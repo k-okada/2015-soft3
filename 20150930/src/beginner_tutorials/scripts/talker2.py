c0cd1504a181801664f6fdd693d010a0d7917ab4
Karasawa <zaki19930927@gmail.com>
2015-10-14 09:27:02 +0900

    fix chatter -> chatter2

--- 20150930/src/beginner_tutorials/scripts/talker2.py
+++ 20150930/src/beginner_tutorials/scripts/talker2.py
@@ -5,7 +5,7 @@ from std_msgs.msg import String
 from beginner_tutorials.msg import Hello
 
 def talker2():
-    pub = rospy.Publisher('chatter',Hello)
+    pub = rospy.Publisher('chatter2',Hello)
     rospy.init_node('talker')
     while not rospy.is_shutdown():
         str = "hello world %s"%rospy.get_time()
