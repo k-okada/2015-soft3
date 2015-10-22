3db3630748ae3d7cc614cb7433fde16b614394ae
Takehiro Hagiwara <thagiwara3@gmail.com>
2015-10-22 21:18:47 +0900

    modified

--- 20151007/src/beginner_tutorials/src/add_two_ints_client.cpp
+++ 20151007/src/beginner_tutorials/src/add_two_ints_client.cpp
@@ -9,15 +9,14 @@ int main(int argc, char **argv){
     return 1;
   }
   ros::NodeHandle n;
-  ros::ServiceClient client
-    = n.serviceClient<beginner_tutorials::AddTwoInts>("add_two_ints");
+  ros::ServiceClient client = n.serviceClient<beginner_tutorials::AddTwoInts>("add_two_ints");
   beginner_tutorials::AddTwoInts srv;
   srv.request.a = atoll(argv[1]);
   srv.request.b = atoll(argv[2]);
   if(client.call(srv)){
     ROS_INFO("Sum: %ld", (long int)srv.response.sum);
   } else {
-    ROS_ERROR("Failed to call service add_two_ints");
+    ROS_INFO("Failed to call service add_two_ints");
     return 1;
   }
   return 0;
