d305bc5f5a8015c8c57c7d7606af80b53ce16682
yuyu2172 <yuyuniitani@gmail.com>
2015-10-26 22:02:29 +0800

    modified talker listener cpp

--- 20151007/src/beginner_tutorials/src/talker.cpp
+++ 20151007/src/beginner_tutorials/src/talker.cpp
@@ -16,6 +16,12 @@ int main(int argc, char **argv)
         std::stringstream ss;
         ss << "hello world" << count;
         msg.data = ss.str();
-        ROS_INFO("%s", 
+        ROS_INFO("%s", msg.data.c_str());
+
+       chatter_pub.publish(msg);
+       ros::spinOnce();
+      loop_rate.sleep();
+     ++count; 
     }    
+    return 0;
 }
