e440eb086179af45e21f62384ee749f932d8ffe5
KosukeArase <k.ara.0605.19107@gmail.com>
2015-10-26 18:02:27 +0900

    不要ファイルを削除

--- 20151007/build/beginner_tutorials/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/vagrant/workspace/ut/2015-soft3/20151007/devel/include".split(';') if "/home/vagrant/workspace/ut/2015-soft3/20151007/devel/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "beginner_tutorials"
-PROJECT_SPACE_DIR = "/home/vagrant/workspace/ut/2015-soft3/20151007/devel"
-PROJECT_VERSION = "0.0.0"
--- 20151007/devel/lib/python2.7/dist-packages/beginner_tutorials/msg/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._DoDishesActionGoal import *
-from ._DoDishesActionResult import *
-from ._DoDishesActionFeedback import *
-from ._DoDishesResult import *
-from ._DoDishesGoal import *
-from ._DoDishesFeedback import *
-from ._DoDishesAction import *
