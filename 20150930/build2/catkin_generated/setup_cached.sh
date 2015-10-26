79efd5045399437bd1733d823f929c54e3996b03
Kosuke Arase <k.arase@conol.jp>
2015-10-26 10:33:27 +0900

    add 2015-10-07 shukudai

---
+++ 20150930/build2/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/vagrant/workspace/ut/2015-soft3/20150930/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/vagrant/workspace/ut/2015-soft3/20150930/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/vagrant/workspace/ut/2015-soft3/20150930/devel/lib:/home/vagrant/workspace/ut/2015-soft3/20150930/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/vagrant/workspace/ut/2015-soft3/20150930/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/vagrant/workspace/ut/2015-soft3/20150930/devel/lib/pkgconfig:/home/vagrant/workspace/ut/2015-soft3/20150930/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/vagrant/workspace/ut/2015-soft3/20150930/build"
+export PYTHONPATH="/home/vagrant/workspace/ut/2015-soft3/20150930/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/vagrant/workspace/ut/2015-soft3/20150930/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/vagrant/workspace/ut/2015-soft3/20150930/src:$ROS_PACKAGE_PATH"
