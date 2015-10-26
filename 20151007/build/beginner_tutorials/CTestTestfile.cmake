917b08813e215c21185da45735758b7f8fd6b964
pazeshun <pazeshun3684@gmail.com>
2015-10-26 15:36:22 +0900

    remove devel and build

--- 20151007/build/beginner_tutorials/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ushun/2015-soft3/20151007/src/beginner_tutorials
-# Build directory: /home/ushun/2015-soft3/20151007/build/beginner_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
--- 20151007/devel/lib/python2.7/dist-packages/beginner_tutorials/msg/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._DoDishesResult import *
-from ._DoDishesFeedback import *
-from ._DoDishesActionFeedback import *
-from ._DoDishesActionGoal import *
-from ._DoDishesGoal import *
-from ._DoDishesActionResult import *
-from ._DoDishesAction import *
