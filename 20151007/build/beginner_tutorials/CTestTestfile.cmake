17de65bb070fa09988ec7403418598d2650a1a00
yuyu2172 <yuyuniitani@gmail.com>
2015-10-26 21:15:56 +0800

    removed files

--- 20151007/build/beginner_tutorials/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/meip-users/2015-soft3/20151007/src/beginner_tutorials
-# Build directory: /home/meip-users/2015-soft3/20151007/build/beginner_tutorials
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
