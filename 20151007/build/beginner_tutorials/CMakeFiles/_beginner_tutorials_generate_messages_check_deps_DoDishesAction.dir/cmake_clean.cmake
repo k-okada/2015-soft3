cf3e785a2df4ed5b3e0bfbf428fced36c45f2f97
TomokiAnzai <chibi314@gmail.com>
2015-10-28 01:37:46 +0900

    通らばリーチ

--- 20151007/build/beginner_tutorials/CMakeFiles/_beginner_tutorials_generate_messages_check_deps_DoDishesAction.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/_beginner_tutorials_generate_messages_check_deps_DoDishesAction"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/_beginner_tutorials_generate_messages_check_deps_DoDishesAction.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
