1c7ca856aba764b73469f206c58b54da31357d06
K.I <katsuma840@gmail.com>
2015-10-27 22:51:56 +0900

    20151014

---
+++ 20151014/build/enshu_20151014/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mech-user/2015-soft3/20151014/src/enshu_20151014
+# Build directory: /home/mech-user/2015-soft3/20151014/build/enshu_20151014
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ 20151014/build/enshu_20151014/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mech-user/2015-soft3/20151014/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/mech-user/2015-soft3/20151014/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/home/mech-user/2015-soft3/20151014/devel/lib/x86_64-linux-gnu:/home/mech-user/2015-soft3/20151007/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/home/mech-user/2015-soft3/20151007/devel/lib:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/home/mech-user/2015-soft3/20151014/devel/lib:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/indigo/lib:/opt/ros/indigo/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages"
+export PATH="/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/home/mech-user/2015-soft3/20151014/devel/bin:/home/mech-user/2015-soft3/20151007/devel/bin:/opt/ros/indigo/bin:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/usr/local/heroku/bin:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/home/mech-user/.rbenv/shims:/home/mech-user/.rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+export PKG_CONFIG_PATH="/home/mech-user/2015-soft3/20151014/devel/lib/x86_64-linux-gnu/pkgconfig:/home/mech-user/2015-soft3/20151007/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/mech-user/2015-soft3/20151007/devel/lib/pkgconfig:/home/mech-user/2015-soft3/20151014/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PYTHONPATH="/home/mech-user/2015-soft3/20151014/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/mech-user/2015-soft3/20151014/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/mech-user/2015-soft3/20151014/src/enshu_20151014:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
