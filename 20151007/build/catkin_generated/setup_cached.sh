48129fe15297a81ce9238e999b1595c6911d21f1
hisyatokaku <hisyatokaku2005@yahoo.co.jp>
2015-10-28 02:16:40 +0900

    add 20151007

---
+++ 20151007/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/mech-user/2015-soft3/20151007/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/mech-user/2015-soft3/20151007/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mech-user/2015-soft3/20151007/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/mech-user/2015-soft3/20151007/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/home/mech-user/2015-soft3/20151007/devel/lib:/home/mech-user/2015-soft3/20151007/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/indigo/lib:/opt/ros/indigo/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages"
+export PATH="/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/home/mech-user/2015-soft3/20151007/devel/bin:/opt/ros/indigo/bin:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/indigo/share/euslisp/jskeus/eus//Linux64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+export PKG_CONFIG_PATH="/home/mech-user/2015-soft3/20151007/devel/lib/pkgconfig:/home/mech-user/2015-soft3/20151007/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/mech-user/2015-soft3/20151007/build"
+export PYTHONPATH="/home/mech-user/2015-soft3/20151007/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/mech-user/2015-soft3/20151007/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/mech-user/2015-soft3/20151007/src/beginner_tutorials:/home/mech-user/2015-soft3/20151007/src:$ROS_PACKAGE_PATH"
