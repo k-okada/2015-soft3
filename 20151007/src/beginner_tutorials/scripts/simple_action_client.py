a75e239f82f6be75ce3a7e5bc9e96bbfa50f067f
ryoheinagao <underthewheel657@gmail.com>
2015-10-28 13:02:45 +0900

    sarani change simple client python file

--- 20151007/src/beginner_tutorials/scripts/simple_action_client.py
+++ 20151007/src/beginner_tutorials/scripts/simple_action_client.py
@@ -9,15 +9,15 @@ from beginner_tutorials.msg import *
 
 if __name__=='__main__':
      rospy.init_node('do_dishes_client')
-     client=actionlib.SimpleActionServer('do_dishes',DoDishesAction)
+     client=actionlib.SimpleActionClient('do_dishes',DoDishesAction)
      client.wait_for_server()
      
-     goal=DoDishes()
+     goal=DoDishesGoal()
      goal.dishwasher_id=1
      print "Returning dishes_cleaned %d"%(result.total_dishes_cleaned)
      
      client.send_goal(goal)
-     client.wait_for_result(rospy.Duration.from_sec(0.5))
+     client.wait_for_result(rospy.Duration.from_sec(5.0))
 
      result = client.get_result()
      print "Resulting dishwasher %d"%(result.total_dishes_cleaned)
