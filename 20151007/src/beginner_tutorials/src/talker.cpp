f561075dcddb933882c0a7daefcdb2d72f92da4e
hayayayaya <sdcckk@gmail.com>
2015-10-28 12:11:17 +0900

    semicolon naoshitayo

--- 20151007/src/beginner_tutorials/src/talker.cpp
+++ 20151007/src/beginner_tutorials/src/talker.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "talker");
     ros::NodeHandle n;
 
-    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000)
+    ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
     ros::Rate loop_rate(1);
 
     int count = 0;
