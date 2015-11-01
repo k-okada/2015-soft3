dc31e4e6e4624d5201a3c42ca870e277bcf820dd
Forest <t.root.16@gmail.com>
2015-11-01 23:01:31 +0900

    modified talker.py and listener.py

--- 20151007/src/beginner_tutorials/scripts/listener.py
+++ 20151007/src/beginner_tutorials/scripts/listener.py
@@ -6,8 +6,9 @@ def callback(data):
     rospy.loginfo(rospy.get_name() + ": I heard %s" % data.data)
 
 def listener():
-    rospy.init_node('listener', anonymous = True)
-    rospy.Subscriber("chatter", String, callback)
+    rospy.init_node('listener',anonymous=True)
+    rospy.Subscriber("chatter",String, callback)
     rospy.spin()
+
 if __name__ == '__main__':
     listener()
