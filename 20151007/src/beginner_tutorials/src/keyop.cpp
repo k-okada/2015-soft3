b20f048b7119997af45b0cceb8a0ca56046e38e8
ShogoMakino <sho5.makino@gmail.com>
2015-11-04 09:19:45 +0900

    別の日の課題をやるため一旦コミット

--- 20151007/src/beginner_tutorials/src/keyop.cpp
+++ 20151007/src/beginner_tutorials/src/keyop.cpp
@@ -3,23 +3,19 @@
 
 int main(int argc, char** argv)
 {
-    if(argc != 4){
-        ROS_INFO("dame!");
-        return 1;
-    }
-    ros::init(argc, argv, "keyop");
-    ros::NodeHandle n;
-    ros::Publisher pub = n.advertise<geometry_msgs::Twist>("mobile_base/commands/velocity", 1000);
-
-    geometry_msgs::Twist twist;
-    twist.linear.x = atof(argv[1]);
-    twist.linear.y = atof(argv[2]);
-    twist.linear.z = atof(argv[3]);
-    twist.angular.x = 0.0;
-    twist.angular.y = 0.0;
-    twist.angular.z = 0.0;
-    pub.publish(twist);
-    ros::spinOnce();
-
-    return 0;
+  ros::init(argc, argv, "keyop");
+  ros::NodeHandle n;
+  ros::Publisher pub = n.advertise<geometry_msgs::Twist>("mobile_base/commands/velocity", 1000);
+  
+  geometry_msgs::Twist twist;
+  twist.linear.x = 0.5;
+  twist.linear.y = 0;
+  twist.linear.z = 0;
+  twist.angular.x = 0.0;
+  twist.angular.y = 0.0;
+  twist.angular.z = 0.0;
+  pub.publish(twist);
+  ros::spinOnce();
+  
+  return 0;
 }
