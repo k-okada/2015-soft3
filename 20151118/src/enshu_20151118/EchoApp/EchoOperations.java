ee07a56417acc52fb965f39e6b39f1fb3f41525a
Jotaro Shigeyama <jojojoe77@gmail.com>
2015-11-27 09:31:19 +0900

    20151118 first commit

---
+++ 20151118/src/enshu_20151118/EchoApp/EchoOperations.java
@@ -0,0 +1,14 @@
+package EchoApp;
+
+
+/**
+* EchoApp/EchoOperations.java .
+* IDL-to-Javaコンパイラ(ポータブル)、バージョン"3.2"によって生成されました
+* echo.idlから
+* 2015年11月25日 16時01分28秒 JST
+*/
+
+public interface EchoOperations 
+{
+  void echoString (String input);
+} // interface EchoOperations
