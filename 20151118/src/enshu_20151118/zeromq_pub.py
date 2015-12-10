d7e1e8f39174d3e6eca69c13f1b000b221f1ec41
pazeshun <pazeshun3684@gmail.com>
2015-12-10 11:40:07 +0900

    add 2015-11-18 shukudai

--- 20151118/src/enshu_20151118/zeromq_pub.py
+++ 20151118/src/enshu_20151118/zeromq_pub.py
@@ -7,10 +7,16 @@ context = zmq.Context()
 publisher = context.socket (zmq.PUB)
 publisher.bind ("tcp://127.0.0.1:8888")
  
-i = 0
+#i = 0
+#while True:
+#    message = str(i) + " hello world"
+#    print(message)
+#    publisher.send (message)
+#    i = 0 if ( i >= 10) else i + 1
+#    time.sleep(0.2)
 while True:
-    message = str(i) + " hello world"
+    lt = time.localtime()
+    message = "Current time is %d/%d/%d %d:%d:%d"%(lt.tm_year, lt.tm_mon, lt.tm_mday, lt.tm_hour, lt.tm_min, lt.tm_sec)
     print(message)
     publisher.send (message)
-    i = 0 if ( i >= 10) else i + 1
-    time.sleep(0.2)
+    time.sleep(1)
