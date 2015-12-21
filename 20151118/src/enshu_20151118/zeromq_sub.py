7257953bbdca55a9b185507c7735b5ffcaf0218e
Your Name <kimhc6028@github.com>
2015-12-22 01:47:25 +0900

    zeromq added.

--- 20151118/src/enshu_20151118/zeromq_sub.py
+++ 20151118/src/enshu_20151118/zeromq_sub.py
@@ -2,12 +2,24 @@
 
 import zmq
 import time
+
 context = zmq.Context()
  
+publisher = context.socket (zmq.PUB)
+publisher.bind("tcp://127.0.0.2:8888")
 subscriber = context.socket (zmq.SUB)
 subscriber.connect ("tcp://127.0.0.1:8888")
-subscriber.setsockopt(zmq.SUBSCRIBE, "4")
- 
+subscriber.setsockopt(zmq.SUBSCRIBE, "neuron1")
+
+j=0
 while True:
-    message = subscriber.recv()
-    print message
+    pub_message = str("neuron2") + str(j)
+    publisher.send(pub_message)
+    sub_message = subscriber.recv()
+    print sub_message
+    j = sub_message[7]
+    j = 9 - int(j)
+#    time.sleep(0.2)
+
+#    print message
+
