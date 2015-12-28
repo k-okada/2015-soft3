dfec28b7e48c29e8412fa446515b13d79e866403
ryoheinagao <underthewheel657@gmail.com>
2015-12-28 17:13:47 +0900

    add 20151118 shukudai

---
+++ 20151118/src/enshu_20151118/EchoApp/Echo.java
@@ -0,0 +1,13 @@
+package EchoApp;
+
+
+/**
+* EchoApp/Echo.java .
+* IDL-to-Javaコンパイラ(ポータブル)、バージョン"3.2"によって生成されました
+* echo.idlから
+* 2015年12月28日 14時23分44秒 JST
+*/
+
+public interface Echo extends EchoOperations, org.omg.CORBA.Object, org.omg.CORBA.portable.IDLEntity 
+{
+} // interface Echo
