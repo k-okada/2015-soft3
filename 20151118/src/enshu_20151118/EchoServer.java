2ea95ae60363cae9d3a63ccda9c5856603a39e24
hisyatokaku <hisyatokaku2005@yahoo.co.jp>
2016-01-10 11:55:58 +0900

    echo-client&server (java,c)

--- 20151118/src/enshu_20151118/EchoServer.java
+++ 20151118/src/enshu_20151118/EchoServer.java
@@ -14,6 +14,7 @@ class EchoImpl extends EchoApp.EchoPOA{
     }
     // implement echoString() method
     public void echoString(String input){
+        System.out.print(input);
     }
     // implement shutdown() method
     public void shutdown(){
