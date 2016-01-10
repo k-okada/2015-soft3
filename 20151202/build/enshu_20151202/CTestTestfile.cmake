ab45b5560c2002303c091e206d36e14e47042368
Karasawa <zaki19930927@gmail.com>
2016-01-11 00:40:23 +0900

    modified2

--- 20151202/build/enshu_20151202/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mech-user/2015-soft3/20151202/src/enshu_20151202
-# Build directory: /home/mech-user/2015-soft3/20151202/build/enshu_20151202
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
